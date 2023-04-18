4a3aa0efb377a1f0064312a7b7cf877cce0b40dc
Athena <athenawilbon1994@aol.com>
2023-04-18 13:53:12 -0400

    models folder updated for tables

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,24 +4,45 @@ const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   // Get all projects and JOIN with user data
-  const projectData = await Project.findAll().catch((err) => {
-    res.json(err);
+
+    try {
+      const projectData = await Project.findAll({
+    include: [
+      {
+        model: User,
+        attributes: ['name'],
+      },
+    ],
   });
+
   const projects = projectData.map((project) => project.get({ plain: true }));
-  res.render("homepage", { projects });
+
+  res.render("homepage", { 
+    projects, 
+    logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get("/project/:id", async (req, res) => {
   try {
-    const projectData = await Project.findByPk(req.params.id);
-    if (!projectData) {
-      res.status(404).json({ message: "No food with this id" });
-      return;
-    }
+    const projectData = await Project.findByPk(req.params.id, {
+    include: [
+      {
+        model: User,
+        attributes: ['name'],
+      },
+    ],
+  });
+    
 
     const project = projectData.get({ plain: true });
 
-    res.render("project", project);
+    res.render("project", {
+      ...project, 
+      logged_in: req.session.logged_in
+    });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -47,14 +68,14 @@ router.get("/project/:id", async (req, res) => {
 //   }
 // });
 
-// router.get("/login", (req, res) => {
-//   // If the user is already logged in, redirect the request to another route
-//   if (req.session.logged_in) {
-//     res.redirect("/profile");
-//     return;
-//   }
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
 
-//   res.render("login");
-// });
+  res.render("login");
+});
 
 module.exports = router;
