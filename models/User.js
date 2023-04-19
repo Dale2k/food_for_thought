34ef3030232e488f3334d8267e1c75488077c4bd
Darryl Subal <dsubal@att.net>
2023-04-19 16:27:27 -0400

    add user routes, update project model, update prject route

--- models/User.js
+++ models/User.js
@@ -28,13 +28,13 @@ User.init(
         isEmail: true,
       },
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8],
-      },
-    },
+    // password: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    //   validate: {
+    //     len: [8],
+    //   },
+    // },
   },
   {
     // hooks: {
