6a6c33aeb05e506c182c7011a77ca2b458c7774c
Darryl Subal <dsubal@att.net>
2023-04-19 11:26:37 -0400

    create project get - post routes

--- controllers/index.js
+++ controllers/index.js
@@ -7,3 +7,22 @@ router.use("/", homeRoutes);
 // router.use('/api', apiRoutes);
 
 module.exports = router;
+
+
+// GET all paperback books
+router.get('/paperbacks', (req, res) => {
+  Book.findAll({
+    // Order by title in ascending order
+    order: ['title'],
+    where: {
+      // Only get books that have this boolean set to TRUE
+      is_paperback: true
+    },
+    attributes: {
+      // Don't include these fields in the returned data
+      exclude: ['is_paperback', 'edition']
+    }
+  }).then((projectData) => {
+    res.json(projectData);
+  });
+});
